513936b44914e16ff9f2ce9b6700709b6bf77845
xwtx-life <2454988619@qq.com>
2026-04-26 23:37:38 +0800

    fix: 使用相对路径并修复静态资源引用

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/Roke-Kingdom-Language-Translator/',
+  base: './',
   plugins: [
     tailwindcss(),
   ],
