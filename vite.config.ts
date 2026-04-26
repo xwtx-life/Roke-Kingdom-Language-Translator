9efa1b88dbba59d7a7ddcbca480a92ebc3ff281b
xwtx-life <2454988619@qq.com>
2026-04-26 23:06:08 +0800

    docs: 配置 GitHub Actions 自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/Roke-Kingdom-Language-Translator/',
   plugins: [
     tailwindcss(),
   ],
